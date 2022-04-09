7f32e0d167a25bbec5374e8e6803d707badb4109
martindavid1995 <48227232+martindavid1995@users.noreply.github.com>
2022-04-09 19:10:30 -0400

    states sending values from Drop to TimeSelector

--- src/Drop.js
+++ src/Drop.js
@@ -2,25 +2,21 @@ import React from "react";
 import "normalize.css";
 import Select from "react-dropdown-select";
 
-
+ 
 
 
 export class Drop extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-        key: this.props.key,
-        options: [] 
-    }
+  send = (key, value) => {
+    this.props.sendValue(key, value)
   }
 
-  render() {
+  render() { 
    
     return (
       <div>
          <Select
           placeholder=""
-          onChange={() => console.log(this.props.options)}
+          onChange={(values) => this.send(this.props.name, values[0].label)}
           values={[]} 
           options={this.props.options}
         />
